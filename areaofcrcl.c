899b6a8e73509f791402b479b76b1efb040762f6
Tulsidas Takekar <156583606+Tulsidas93@users.noreply.github.com>
2024-01-29 20:07:52 +0530

    areaofcrcl.c
    Create

---
+++ areaofcrcl.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+float radius, result;
+printf("enter radius of circle:\n");
+scanf("%f",&radius);
+result = (3.14159 * radius * radius);
+printf("Area of circle Sis :%f\n", result);
+return 0;
+}
